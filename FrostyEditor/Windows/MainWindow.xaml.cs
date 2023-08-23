67c56b18f3e4ca0055a3a7e8bbeb8b55d4c4bd92
mpanetta <mpanetta755@gmail.com>
2023-08-22 21:43:38 -0400

    Adding M24 built-in datapath fix.

--- FrostyEditor/Windows/MainWindow.xaml.cs
+++ FrostyEditor/Windows/MainWindow.xaml.cs
@@ -1004,7 +1004,7 @@ namespace FrostyEditor.Windows
             if (!string.IsNullOrEmpty(selectedFolder))
             {
                 // Look for the specified folder and files and delete them if found
-                string[] itemsToDelete = { "ModData", "tmp", "CryptBase.dll", "Madden24.old" };
+                string[] itemsToDelete = { "ModData", "tmp", "CryptBase.dll", "Madden24.old", "dpapi.dll", "datapath", "datapath.old" };
                 bool foundAndDeleted = false;
                 foreach (string item in itemsToDelete)
                 {
